b6574bbf1aa95eb22d0bf412c4add2468369612b
1612052 <chienpham304@gmail.com>
2019-05-17 18:21:34 +0700

    add density to resolve

--- app/src/main/java/com/chienpm/safecharge/LockscreenActivity.java
+++ app/src/main/java/com/chienpm/safecharge/LockscreenActivity.java
@@ -50,7 +50,7 @@ public class LockscreenActivity extends AppCompatActivity {
     private String mNextPattern="";
     private int wrongCount = 5;
 
-    //Todo: ReDefine layout to fit all kind of device
+    //Todo: ReDefine layout to fit all kind of device, hdpi, lpdi, mpdi, xhdpi
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
